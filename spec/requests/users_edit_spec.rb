751d0a3786d83d6dadde84caaf3f3835d167a775
Keita Nishimura <fast.break.baske03@gmail.com>
2020-07-26 22:28:21 +0900

    test:ユーザー編集機能のテスト

---
+++ spec/requests/users_edit_spec.rb
@@ -0,0 +1,31 @@
+require "rails_helper"
+
+RSpec.describe "プロフィール編集", type: :request do
+  let!(:user) { create(:user) }
+
+  before do
+    get edit_user_path(user)
+  end
+
+  it "有効なプロフィール編集" do
+    expect(response).to render_template('users/edit')
+    patch user_path(user), params: { user: {
+      name: "Example User",
+      email: "user@example.com",
+      password: "",
+      password_confirmation: ""
+    } }
+    redirect_to user
+    follow_redirect!
+    expect(response).to render_template('users/show')
+  end
+
+  it "無効なプロフィール編集" do
+    expect(response).to render_template('users/edit')
+    patch user_path(user), params: { user: {
+      name: "",
+      email: "xxx@invalid"
+    } }
+    expect(response).to render_template('users/edit')
+  end
+end
