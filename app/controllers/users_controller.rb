01e404a17bdd0bb84e5333f5fb2ba1725c5894d0
Keita Nishimura <fast.break.baske03@gmail.com>
2020-07-27 10:40:41 +0900

    perf:適切なユーザーに適切なアクションを行わせる設定を追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:index, :show, :edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
+
   def new
     @user = User.new
   end
@@ -40,5 +43,14 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation,
                                  :sex, :introduction, :picture)
+  end
+
+  # 正しいユーザーかどうか確認
+  def correct_user
+    @user = User.find(params[:id])
+    if !current_user?(@user)
+      flash[:danger] = "このページへはアクセスできません"
+      redirect_to(root_url)
+    end
   end
 end
