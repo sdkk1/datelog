2baeb71716fcaa68c6181067c5e483e2cb1a07bd
Keita Nishimura <fast.break.baske03@gmail.com>
2020-08-04 23:19:59 +0900

    test:投稿機能のテスト

--- spec/requests/datespots_record_spec.rb
+++ spec/requests/datespots_record_spec.rb
@@ -5,10 +5,42 @@ RSpec.describe "投稿", type: :request do
   let!(:datespot) { create(:datespot, user: user) }
 
   context "ログインしているユーザーの場合" do
-    it "レスポンスが正常に表示されること" do
+    before do
       login_for_request(user)
       get new_datespot_path
+    end
+
+    it "レスポンスが正常に表示されること" do
       expect(response).to have_http_status "200"
+      expect(response).to render_template('datespots/new')
+    end
+
+    it "有効な料理データで登録できること" do
+      expect {
+        post datespots_path, params: { datespot: {
+          name: "ももたろう",
+          area: "恵比寿",
+          price: "1万円〜",
+          keyword: "焼き鳥",
+          point: "シックな店内で落ち着いた雰囲気のお店",
+          caution: "お酒の種類は少ない",
+        } }
+      }.to change(Datespot, :count).by(1)
+      follow_redirect!
+      expect(response).to render_template('datespots/index')
+    end
+
+    it "無効な料理データでは登録できないこと" do
+      expect {
+        post datespots_path, params: { datespot: {
+          name: "",
+          area: "恵比寿",
+          price: "1万円〜",
+          keyword: "焼き鳥",
+          point: "シックな店内で落ち着いた雰囲気のお店",
+          caution: "お酒の種類は少ない",
+        } }
+      }.not_to change(Datespot, :count)
       expect(response).to render_template('datespots/new')
     end
   end
--- spec/system/datespots_spec.rb
+++ spec/system/datespots_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe "Datespots", type: :system do
+  let!(:user) { create(:user) }
+
   describe "投稿一覧ページ" do
     before do
       create_list(:datespot, 10)
@@ -32,6 +34,56 @@ RSpec.describe "Datespots", type: :system do
 
       it "デートスポットのページネーションが表示されていることを確認" do
         expect(page).to have_css "div.pagination"
+      end
+    end
+  end
+
+  describe "投稿ページ" do
+    before do
+      login_for_system(user)
+      visit new_datespot_path
+    end
+
+    context "ページレイアウト" do
+      it "「投稿する」の文字列が存在すること" do
+        expect(page).to have_content '投稿する'
+      end
+
+      it "正しいタイトルが表示されること" do
+        expect(page).to have_title full_title('投稿')
+      end
+
+      it "入力部分に適切なラベルが表示されること" do
+        expect(page).to have_content '店名'
+        expect(page).to have_content 'エリア'
+        expect(page).to have_content '価格帯'
+        expect(page).to have_content 'キーワード'
+        expect(page).to have_content 'ポイント'
+        expect(page).to have_content '注意点'
+      end
+    end
+
+    context "投稿する処理" do
+      it "有効な情報で投稿するを行うと投稿する成功のフラッシュが表示されること" do
+        fill_in "店名", with: "ももたろう"
+        fill_in "エリア", with: "恵比寿"
+        fill_in "価格帯", with: "1万円〜"
+        fill_in "キーワード", with: "焼き鳥"
+        fill_in "ポイント", with: "シックな店内で落ち着いた雰囲気のお店"
+        fill_in "注意点", with: "お酒の種類は少ない"
+        click_button "登録する"
+        expect(page).to have_content "投稿が登録されました！"
+      end
+
+      it "無効な情報で投稿するを行うと投稿する失敗のフラッシュが表示されること" do
+        fill_in "店名", with: ""
+        fill_in "エリア", with: "恵比寿"
+        fill_in "価格帯", with: "1万円〜"
+        fill_in "キーワード", with: "焼き鳥"
+        fill_in "ポイント", with: "シックな店内で落ち着いた雰囲気のお店"
+        fill_in "注意点", with: "お酒の種類は少ない"
+        click_button "登録する"
+        expect(page).to have_content "店名を入力してください"
       end
     end
   end
