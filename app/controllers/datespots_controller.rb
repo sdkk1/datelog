ef737324b8e0ad92c1ba11414cf7e6212f9a2e98
Keita Nishimura <fast.break.baske03@gmail.com>
2020-08-04 22:51:44 +0900

    feat:投稿機能の実装

--- app/controllers/datespots_controller.rb
+++ app/controllers/datespots_controller.rb
@@ -8,4 +8,21 @@ class DatespotsController < ApplicationController
   def index
     @datespots = Datespot.paginate(page: params[:page], per_page: 5)
   end
+
+  def create
+    @datespot = current_user.datespots.build(datespot_params)
+    if @datespot.save
+      flash[:success] = "投稿が登録されました！"
+      redirect_to datespots_url
+    else
+      render 'datespots/new'
+    end
+  end
+
+  private
+
+  def datespot_params
+    params.require(:datespot).permit(:name, :area, :price,
+                                     :keyword, :point, :caution)
+  end
 end
