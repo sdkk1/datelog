5f742f9bdc7a440d83fd67406544030ca54e6e15
Keita Nishimura <fast.break.baske03@gmail.com>
2020-08-07 17:57:04 +0900

    test:投稿削除機能のテスト

--- spec/system/datespots_spec.rb
+++ spec/system/datespots_spec.rb
@@ -1,16 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe "Datespots", type: :system do
+  let!(:admin_user) { create(:user, :admin) }
   let!(:user) { create(:user) }
   let!(:datespot) { create(:datespot, user: user) }
 
   describe "投稿一覧ページ" do
     before do
       create_list(:datespot, 10)
-      visit datespots_path
     end
 
-    context "ページレイアウト" do
+    context "ページレイアウト(共通)" do
+      before do
+        visit datespots_path
+      end
+
       it "「投稿一覧」の文字列が存在することを確認" do
         expect(page).to have_content '投稿一覧'
       end
@@ -28,12 +32,13 @@ RSpec.describe "Datespots", type: :system do
       end
     end
 
-    context "デートスポットの情報(認可されたユーザーの場合)" do
+    context "ページレイアウト(管理者ユーザーの場合)" do
       before do
-        login_for_system(user)
+        login_for_system(admin_user)
+        visit datespots_path
       end
 
-      it "デートスポットの情報が表示されていることを確認" do
+      it "デートスポットの情報が表示されていることを確認(投稿者のリンクあり)" do
         Datespot.take(5).each do |datespot|
           expect(page).to have_link datespot.name
           expect(page).to have_content datespot.area
@@ -42,16 +47,85 @@ RSpec.describe "Datespots", type: :system do
           expect(page).to have_link datespot.user.name
         end
       end
+
+      it "全ての投稿に削除ボタンが表示されること" do
+        Datespot.take(5).each do |datespot|
+          expect(page).to have_link "削除"
+        end
+      end
     end
 
-    context "デートスポットの情報(ログインしていないユーザーの場合)" do
-      it "デートスポットの情報が表示されていることを確認" do
+    context "投稿削除(管理者ユーザーの場合)", js: true do
+      before do
+        login_for_system(admin_user)
+        visit datespots_path
+      end
+
+      it "投稿を削除後、削除成功のフラッシュが表示されること" do
+        page.accept_confirm("本当に削除しますか？") do
+          click_on "削除", match: :first
+        end
+        expect(page).to have_content '投稿が削除されました'
+      end
+    end
+
+    context "ページレイアウト(管理者ユーザー以外の場合)" do
+      before do
+        login_for_system(user)
+        visit datespots_path
+      end
+
+      it "デートスポットの情報が表示されていることを確認(投稿者のリンクあり)" do
+        Datespot.take(5).each do |datespot|
+          expect(page).to have_link datespot.name
+          expect(page).to have_content datespot.area
+          expect(page).to have_content datespot.price
+          expect(page).to have_content datespot.keyword
+          expect(page).to have_link datespot.user.name
+        end
+      end
+
+      it "自分の投稿のみ削除ボタンが表示されること" do
+        Datespot.take(5).each do |datespot|
+          expect(page).to have_link "削除" if datespot == user.datespots
+        end
+      end
+    end
+
+    context "投稿削除(管理者ユーザー以外の場合)", js: true do
+      before do
+        login_for_system(user)
+        visit datespots_path
+      end
+
+      it "自分の投稿を削除後、削除成功のフラッシュが表示されること" do
+        if datespot == user.datespots
+          page.accept_confirm("本当に削除しますか？") do
+            click_on "削除", match: :first
+          end
+          expect(page).to have_content '投稿が削除されました'
+        end
+      end
+    end
+
+    context "ページレイアウト(ログインしていないユーザーの場合)" do
+      before do
+        visit datespots_path
+      end
+
+      it "デートスポットの情報が表示されていることを確認(投稿者のリンクなし)" do
         Datespot.take(5).each do |datespot|
           expect(page).to have_link datespot.name
           expect(page).to have_content datespot.area
           expect(page).to have_content datespot.price
           expect(page).to have_content datespot.keyword
           expect(page).to have_content datespot.user.name
+        end
+      end
+
+      it "削除ボタンが表示されないこと" do
+        Datespot.take(5).each do |datespot|
+          expect(page).to have_no_link "削除"
         end
       end
     end
@@ -108,16 +182,91 @@ RSpec.describe "Datespots", type: :system do
   end
 
   describe "投稿詳細ページ" do
-    context "ページレイアウト" do
+    context "ページレイアウト（共通）" do
+      it "正しいタイトルが表示されること" do
+        visit datespot_path(datespot)
+        expect(page).to have_title full_title("#{datespot.name}")
+      end
+    end
+
+    context "ページレイアウト(管理者ユーザーの場合)" do
+      before do
+        login_for_system(admin_user)
+        visit datespot_path(datespot)
+      end
+
+      it "投稿情報が表示されること(投稿者のリンクあり)" do
+        expect(page).to have_content datespot.name
+        expect(page).to have_link datespot.user.name
+        expect(page).to have_content datespot.area
+        expect(page).to have_content datespot.price
+        expect(page).to have_content datespot.keyword
+        expect(page).to have_content datespot.point
+        expect(page).to have_content datespot.caution
+      end
+
+      it "削除ボタンが表示されること" do
+        expect(page).to have_link "削除"
+      end
+    end
+
+    context "投稿削除(管理者ユーザーの場合)", js: true do
+      before do
+        login_for_system(admin_user)
+        visit datespot_path(datespot)
+      end
+
+      it "投稿を削除後、削除成功のフラッシュが表示されること" do
+        page.accept_confirm("本当に削除しますか？") do
+          click_on "削除"
+        end
+        expect(page).to have_content '投稿が削除されました'
+      end
+    end
+
+    context "ページレイアウト(管理者ユーザー以外の場合)" do
+      before do
+        login_for_system(user)
+        visit datespot_path(datespot)
+      end
+
+      it "投稿情報が表示されること(投稿者のリンクあり)" do
+        expect(page).to have_content datespot.name
+        expect(page).to have_link datespot.user.name
+        expect(page).to have_content datespot.area
+        expect(page).to have_content datespot.price
+        expect(page).to have_content datespot.keyword
+        expect(page).to have_content datespot.point
+        expect(page).to have_content datespot.caution
+      end
+
+      it "自分の投稿のみ削除ボタンが表示されること" do
+        expect(page).to have_link "削除" if datespot == user.datespots
+      end
+    end
+
+    context "投稿削除(管理者ユーザー以外の場合)", js: true do
+      before do
+        login_for_system(user)
+        visit datespot_path(datespot)
+      end
+
+      it "自分の投稿を削除後、削除成功のフラッシュが表示されること" do
+        if datespot == user.datespots
+          page.accept_confirm("本当に削除しますか？") do
+            click_on "削除"
+          end
+          expect(page).to have_content '投稿が削除されました'
+        end
+      end
+    end
+
+    context "ページレイアウト(ログインしていないユーザーの場合)" do
       before do
         visit datespot_path(datespot)
       end
 
-      it "正しいタイトルが表示されること" do
-        expect(page).to have_title full_title("#{datespot.name}")
-      end
-
-      it "投稿情報が表示されること" do
+      it "投稿情報が表示されること(投稿者のリンクなし)" do
         expect(page).to have_content datespot.name
         expect(page).to have_content datespot.user.name
         expect(page).to have_content datespot.area
@@ -125,6 +274,10 @@ RSpec.describe "Datespots", type: :system do
         expect(page).to have_content datespot.keyword
         expect(page).to have_content datespot.point
         expect(page).to have_content datespot.caution
+      end
+
+      it "削除ボタンが表示されないこと" do
+        expect(page).to have_no_link "削除"
       end
     end
   end
@@ -174,6 +327,15 @@ RSpec.describe "Datespots", type: :system do
         click_button "更新する"
         expect(page).to have_content '店名を入力してください'
         expect(datespot.reload.name).not_to eq ""
+      end
+    end
+
+    context "投稿削除", js: true do
+      it "投稿を削除後、削除成功のフラッシュが表示されること" do
+        page.accept_confirm("本当にこの投稿を削除してもよろしいですか？") do
+          click_on "投稿を削除する"
+        end
+        expect(page).to have_content '投稿が削除されました'
       end
     end
   end
--- spec/system/static_pages_spec.rb
+++ spec/system/static_pages_spec.rb
@@ -1,15 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe "StaticPages", type: :system do
+  let!(:admin_user) { create(:user, :admin) }
   let!(:user) { create(:user) }
+  let!(:datespot) { create(:datespot, user: user) }
 
-  describe "HomePage" do
+  describe "トップページ" do
     before do
       create_list(:datespot, 10)
-      visit root_path
     end
 
-    context "ページレイアウト" do
+    context "ページレイアウト(共通)" do
+      before do
+        visit root_path
+      end
+
       it "datelogの文字列が存在することを確認" do
         expect(page).to have_content 'datelog'
       end
@@ -27,12 +32,13 @@ RSpec.describe "StaticPages", type: :system do
       end
     end
 
-    context "デートスポットの情報(認可されたユーザーの場合)" do
+    context "ページレイアウト(管理者ユーザーの場合)" do
       before do
-        login_for_system(user)
+        login_for_system(admin_user)
+        visit root_path
       end
 
-      it "デートスポットの情報が表示されていることを確認" do
+      it "デートスポットの情報が表示されていることを確認(投稿者のリンクあり)" do
         Datespot.take(5).each do |datespot|
           expect(page).to have_link datespot.name
           expect(page).to have_content datespot.area
@@ -41,16 +47,85 @@ RSpec.describe "StaticPages", type: :system do
           expect(page).to have_link datespot.user.name
         end
       end
+
+      it "全ての投稿に削除ボタンが表示されること" do
+        Datespot.take(5).each do |datespot|
+          expect(page).to have_link "削除"
+        end
+      end
     end
 
-    context "デートスポットの情報(ログインしていないユーザーの場合)" do
-      it "デートスポットの情報が表示されていることを確認" do
+    context "投稿削除(管理者ユーザーの場合)", js: true do
+      before do
+        login_for_system(admin_user)
+        visit root_path
+      end
+
+      it "投稿を削除後、削除成功のフラッシュが表示されること" do
+        page.accept_confirm("本当に削除しますか？") do
+          click_on "削除", match: :first
+        end
+        expect(page).to have_content '投稿が削除されました'
+      end
+    end
+
+    context "ページレイアウト(管理者ユーザー以外の場合)" do
+      before do
+        login_for_system(user)
+        visit root_path
+      end
+
+      it "デートスポットの情報が表示されていることを確認(投稿者のリンクあり)" do
+        Datespot.take(5).each do |datespot|
+          expect(page).to have_link datespot.name
+          expect(page).to have_content datespot.area
+          expect(page).to have_content datespot.price
+          expect(page).to have_content datespot.keyword
+          expect(page).to have_link datespot.user.name
+        end
+      end
+
+      it "自分の投稿のみ削除ボタンが表示されること" do
+        Datespot.take(5).each do |datespot|
+          expect(page).to have_link "削除" if datespot == user.datespots
+        end
+      end
+    end
+
+    context "投稿削除(管理者ユーザー以外の場合)", js: true do
+      before do
+        login_for_system(user)
+        visit root_path
+      end
+
+      it "自分の投稿を削除後、削除成功のフラッシュが表示されること" do
+        if datespot == user.datespots
+          page.accept_confirm("本当に削除しますか？") do
+            click_on "削除", match: :first
+          end
+          expect(page).to have_content '投稿が削除されました'
+        end
+      end
+    end
+
+    context "ページレイアウト(ログインしていないユーザーの場合)" do
+      before do
+        visit root_path
+      end
+
+      it "デートスポットの情報が表示されていることを確認(投稿者のリンクなし)" do
         Datespot.take(5).each do |datespot|
           expect(page).to have_link datespot.name
           expect(page).to have_content datespot.area
           expect(page).to have_content datespot.price
           expect(page).to have_content datespot.keyword
           expect(page).to have_content datespot.user.name
+        end
+      end
+
+      it "削除ボタンが表示されないこと" do
+        Datespot.take(5).each do |datespot|
+          expect(page).to have_no_link "削除"
         end
       end
     end
