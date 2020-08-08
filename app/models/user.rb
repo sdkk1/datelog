0f08d64ecc8b634b5781f2bccd50703cdfa4f52b
Keita Nishimura <fast.break.baske03@gmail.com>
2020-08-08 21:50:29 +0900

    feat:Relationshipモデルの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
   has_many :datespots, dependent: :destroy
+  has_many :active_relationships, class_name: "Relationship",
+                                  foreign_key: "follower_id",
+                                  dependent: :destroy
+
   before_save :downcase_email
 
   validates :name, presence: true, length: { maximum: 50 }
