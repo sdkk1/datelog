be9ea9f080c8631d057988b4cca204b6fb1aca79
Keita Nishimura <fast.break.baske03@gmail.com>
2020-08-09 12:16:51 +0900

    feat:フォロー機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,11 @@ class User < ApplicationRecord
   has_many :active_relationships, class_name: "Relationship",
                                   foreign_key: "follower_id",
                                   dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :passive_relationships, class_name: "Relationship",
+                                   foreign_key: "followed_id",
+                                   dependent: :destroy
+  has_many :followers, through: :passive_relationships, source: :follower
 
   before_save :downcase_email
 
@@ -22,6 +27,26 @@ class User < ApplicationRecord
 
   mount_uploader :picture, PictureUploader
   validate :picture_size
+
+  # ユーザーをフォローする
+  def follow(other_user)
+    following << other_user
+  end
+
+  # ユーザーをフォロー解除する
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  # 現在のユーザーがフォローしてたらtrueを返す
+  def following?(other_user)
+    following.include?(other_user)
+  end
+
+  # 現在のユーザーがフォローされていたらtrueを返す
+  def followed_by?(other_user)
+    followers.include?(other_user)
+  end
 
   private
 
