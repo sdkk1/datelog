3bf465cc059f18fa47189422129a572df59c39be
Keita Nishimura <fast.break.baske03@gmail.com>
2020-08-10 11:59:20 +0900

    feat:Favoriteモデルを作成

--- app/models/datespot.rb
+++ app/models/datespot.rb
@@ -1,5 +1,7 @@
 class Datespot < ApplicationRecord
   belongs_to :user
+  has_many :favorites, dependent: :destroy
+
   default_scope -> { order(created_at: :desc) }
 
   validates :user_id, presence: true
