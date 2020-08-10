7e9851cf7d5e3331bf0985d93d66fb9ee0f148e3
Keita Nishimura <fast.break.baske03@gmail.com>
2020-08-10 22:18:43 +0900

    feat:お気に入り一覧ページの作成

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -2,6 +2,8 @@ class Favorite < ApplicationRecord
   belongs_to :user
   belongs_to :datespot
 
+  default_scope -> { order(created_at: :desc) }
+
   validates :user_id, presence: true
   validates :datespot_id, presence: true
 end
