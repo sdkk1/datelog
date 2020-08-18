aeae6d17287f2ce726744e82edcc6c6b92d3db57
Keita Nishimura <fast.break.baske03@gmail.com>
2020-08-18 22:06:45 +0900

    test:Notificationモデルのモデルテストを作成

--- spec/factories/notifications.rb
+++ spec/factories/notifications.rb
@@ -1,9 +1,10 @@
 FactoryBot.define do
   factory :notification do
-    user_id 1
-    datespot_id 1
-    variety 1
-    content "MyText"
-    from_user_id 1
+    datespot_id { 1 }
+    variety { 1 }
+    content { "" }
+    from_user_id { 2 }
+    association :user
+    association :datespot
   end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -8,6 +8,7 @@ require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 require 'capybara/rspec'
 require 'carrierwave/test/matchers'
+require 'rspec/retry'
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
@@ -65,4 +66,14 @@ RSpec.configure do |config|
   # config.filter_gems_from_backtrace("gem name")
   config.include FactoryBot::Syntax::Methods
   config.include Devise::Test::IntegrationHelpers, type: :system
+
+  # 実行中にリトライのステータスを表示する
+  config.verbose_retry = true
+  # リトライの原因となった例外を表示する
+  config.display_try_failure_messages = true
+
+  # js: true のフィーチャスペックのみリトライを有効にする
+  config.around :each, :js do |ex|
+    ex.run_with_retry retry: 3
+  end
 end
