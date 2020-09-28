87725e172d0d9ecd0db980cb281f4d4992d3c6e3
Keita Nishimura <fast.break.baske03@gmail.com>
2020-09-28 14:44:01 +0900

    feat:各口コミの評価と平均評価の表示

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -7,5 +7,5 @@ class Comment < ApplicationRecord
   validates :user_id, presence: true
   validates :datespot_id, presence: true
   validates :content, presence: true, length: { maximum: 255 }
-  validates :rate, numericality: { less_than_or_equal_to: 5, greater_than_or_equal_to: 1 }, presence: true
+  validates :rate, numericality: { less_than_or_equal_to: 5, greater_than_or_equal_to: 0.5 }, presence: true
 end
