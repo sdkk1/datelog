3d515b93d94f2576ca11565aed99963155aa5e0c
Keita Nishimura <fast.break.baske03@gmail.com>
2020-10-15 21:18:23 +0900

    feat:マッチング一覧ページとメッセージ一覧ページの作成

---
+++ app/controllers/rooms_controller.rb
@@ -0,0 +1,31 @@
+class RoomsController < ApplicationController
+  before_action :logged_in_user
+
+  def index
+    @members = current_user.matchers
+  end
+
+  def create
+    @room = Room.create
+    @join_current_user = Entry.create(user_id: current_user.id, room_id: @room.id)
+    @join_user = Entry.create(join_room_params)
+    redirect_to room_path(@room.id)
+  end
+
+  def show
+    @room = Room.find(params[:id])
+    if Entry.where(user_id: current_user.id, room_id: @room.id).present?
+      @messages = @room.messages.includes(:user).order("created_at asc")
+      @message = Message.new
+      @entries = @room.entries
+    else
+      redirect_back(fallback_location: root_path)
+    end
+  end
+
+  private
+
+  def join_room_params
+    params.require(:entry).permit(:user_id, :room_id).merge(room_id: @room.id)
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -67,6 +67,11 @@ class User < ApplicationRecord
     followers.include?(other_user)
   end
 
+  # マッチングしたユーザー
+  def matchers
+    following & followers
+  end
+
   # 投稿をお気に入り登録する
   def favorite(datespot)
     Favorite.create!(user_id: id, datespot_id: datespot.id)
