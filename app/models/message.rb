1293a0f95315b8b350a89d0da5946b692f8b366f
Keita Nishimura <fast.break.baske03@gmail.com>
2020-10-21 23:24:59 +0900

    feat:DMで写真を送信する時にバリデーションエラーメッセージを表示する機能

--- app/models/message.rb
+++ app/models/message.rb
@@ -5,4 +5,23 @@ class Message < ApplicationRecord
   validates :content, presence: true, length: { maximum: 255 }
 
   has_one_attached :picture
+  validate :picture_type, :picture_size
+
+  private
+
+  def picture_type
+    return unless picture.attached?
+    if !picture.blob.content_type.in?(%('image/jpeg image/png'))
+      picture.purge
+      errors.add(:picture, 'はjpegまたはpng形式でアップロードしてください')
+    end
+  end
+
+  def picture_size
+    return unless picture.attached?
+    if picture.blob.byte_size > 5.megabytes
+      picture.purge
+      errors.add(:picture, "は1つのファイル5MB以内にしてください")
+    end
+  end
 end
