5553508b3b8d463483a2c106977a73e1dccda4fd
Keita Nishimura <fast.break.baske03@gmail.com>
2020-11-14 20:26:44 +0900

    fix:set :ssh_optionsをconfig/deploy.rbへ移動

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,5 +1,2 @@
 # EC2サーバーのIP、EC2サーバーにログインするユーザー名、サーバーのロールを記述
-server ' 52.197.170.143', user: 'keita', roles: %w{app db web}
-
-#デプロイするサーバーにsshログインする鍵の情報を記述
-set :ssh_options, keys: '~/.ssh/date-match_key_rsa'
+server '52.197.170.143', user: 'keita', roles: %w{app db web}
