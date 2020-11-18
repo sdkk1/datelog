69302a16b812160e06ce59d24744f0d08d1e4f5d
Keita Nishimura <fast.break.baske03@gmail.com>
2020-11-18 23:38:54 +0900

    fix:config/deploy/production.rbにssh接続するコードを追加

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,2 +1,9 @@
 # EC2サーバーのIP、EC2サーバーにログインするユーザー名、サーバーのロールを記述
 server '52.197.170.143', user: 'keita', roles: %w{app db web}
+
+# CircleCIのGUIで設定した環境変数を使ってSSH接続
+set :ssh_options, {
+  keys: [ENV.fetch('PRODUCTION_SSH_KEY').to_s],
+  forward_agent: true,
+  auth_methods: %w[publickey]
+}
