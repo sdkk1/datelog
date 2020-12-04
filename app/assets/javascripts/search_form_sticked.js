227f6d7ed1871e8a5c3d1c78ae1c3552d1cf4606
Keita Nishimura <fast.break.baske03@gmail.com>
2020-12-04 15:03:14 +0900

    feat:投稿検索フォームのデザインを作成

---
+++ app/assets/javascripts/search_form_sticked.js
@@ -0,0 +1,38 @@
+$(function($){
+  var row = $(".row");
+  var content = $(".content");
+  var search_form_sticked = $(".search_form_sticked");
+
+  // コンテンツの位置
+  var content_top = content.offset().top;
+  // 固定する要素の元々の位置
+  var search_form_sticked_original_top = search_form_sticked.offset().top;
+  // 固定する要素の高さ
+  var search_form_sticked_height = search_form_sticked.height();
+
+  // スクロールかリサイズ時
+  $(window).on('scroll resize', function(){
+      // 現在の位置
+      var scrollTop = $(document).scrollTop();
+      // メイン要素の最後尾
+      var row_bottom = row.offset().top + row.height();
+      // 現在の位置が、初期位置より下で、メイン要素の最後尾より上なら、画面上部に検索フォームを固定
+      if ((scrollTop > search_form_sticked_original_top) && (scrollTop < row_bottom - search_form_sticked_height)){
+          search_form_sticked.css({'position': 'fixed',
+              'top': 0,
+              'width': content.width()
+          });
+      } else if(scrollTop >= row_bottom - search_form_sticked_height){
+          // 現在位置がメイン要素の最後尾より下なら、メイン要素の最後尾に検索フォームを位置させる
+          search_form_sticked.css({'position': 'absolute',
+              'top': row_bottom - search_form_sticked_height - content_top,
+              'width': content.width()
+          });
+      } else {
+          // 現在位置が初期位置より上なら、幅だけリサイズする
+          search_form_sticked.css({'position': 'static',
+              'width': content.width()
+          });
+      }
+  });
+});
--- app/assets/javascripts/sidebar_sticked.js
+++ app/assets/javascripts/sidebar_sticked.js
@@ -1,36 +1,36 @@
 $(function($){
-  var content = $(".row");
+  var row = $(".row");
   var sidebar = $(".sidebar");
-  var sticked = $(".sticked");
+  var sidebar_sticked = $(".sidebar_sticked");
 
   // サイドバーの位置
   var sidebar_top = sidebar.offset().top;
   // 固定する要素の元々の位置
-  var sticked_original_top = sticked.offset().top;
+  var sidebar_sticked_original_top = sidebar_sticked.offset().top;
   // 固定する要素の高さ
-  var sticked_height = sticked.height();
+  var sidebar_sticked_height = sidebar_sticked.height();
 
   // スクロールかリサイズ時
   $(window).on('scroll resize', function(){
       // 現在の位置
       var scrollTop = $(document).scrollTop();
       // メイン要素の最後尾
-      var content_bottom = content.offset().top + content.height();
+      var row_bottom = row.offset().top + row.height();
       // 現在の位置が、初期位置より下で、メイン要素の最後尾より上なら、画面上部にサイドバーを固定
-      if ((scrollTop > sticked_original_top) && (scrollTop < content_bottom - sticked_height)){
-          sticked.css({'position': 'fixed',
+      if ((scrollTop > sidebar_sticked_original_top) && (scrollTop < row_bottom - sidebar_sticked_height)){
+          sidebar_sticked.css({'position': 'fixed',
               'top': 0,
               'width': sidebar.width()
           });
-      } else if(scrollTop >= content_bottom - sticked_height){
+      } else if(scrollTop >= row_bottom - sidebar_sticked_height){
           // 現在位置がメイン要素の最後尾より下なら、メイン要素の最後尾にサイドバーを位置させる
-          sticked.css({'position': 'absolute',
-              'top': content_bottom - sticked_height - sidebar_top,
+          sidebar_sticked.css({'position': 'absolute',
+              'top': row_bottom - sidebar_sticked_height - sidebar_top,
               'width': sidebar.width()
           });
       } else {
           // 現在位置が初期位置より上なら、幅だけリサイズする
-          sticked.css({'position': 'static',
+          sidebar_sticked.css({'position': 'static',
               'width': sidebar.width()
           });
       }
