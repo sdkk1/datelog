367565f7cb7ca804c363f9bea17a1b42a16b13dc
Keita Nishimura <fast.break.baske03@gmail.com>
2021-01-07 11:40:43 +0900

    docs:投稿一覧ページのデザインを微修正

--- app/controllers/datespots_controller.rb
+++ app/controllers/datespots_controller.rb
@@ -33,16 +33,16 @@ class DatespotsController < ApplicationController
   def index
     if params[:q].present?
       @search = Datespot.ransack(params[:q])
-      @datespots = @search.result.with_attached_images.preload(:user, :taggings, :comments).paginate(page: params[:page], per_page: 15)
+      @datespots = @search.result.with_attached_images.preload(:user, :taggings, :comments).paginate(page: params[:page], per_page: 9)
       if params[:tag_name]
-        @datespots = @search.result.tagged_with("#{params[:tag_name]}").with_attached_images.preload(:user, :taggings).paginate(page: params[:page], per_page: 15)
+        @datespots = @search.result.tagged_with("#{params[:tag_name]}").with_attached_images.preload(:user, :taggings).paginate(page: params[:page], per_page: 9)
       end
     else
       params[:q] = { sorts: 'updated_at desc' }
       @search = Datespot.ransack(params[:q])
-      @datespots = @search.result.with_attached_images.preload(:user, :taggings, :comments).paginate(page: params[:page], per_page: 15)
+      @datespots = @search.result.with_attached_images.preload(:user, :taggings, :comments).paginate(page: params[:page], per_page: 9)
       if params[:tag_name]
-        @datespots = @search.result.tagged_with("#{params[:tag_name]}").with_attached_images.preload(:user, :taggings).paginate(page: params[:page], per_page: 15)
+        @datespots = @search.result.tagged_with("#{params[:tag_name]}").with_attached_images.preload(:user, :taggings).paginate(page: params[:page], per_page: 9)
       end
     end
   end
