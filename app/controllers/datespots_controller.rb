94bef24fc3bd532160af2f2c18cbafeec74d54e4
Keita Nishimura <fast.break.baske03@gmail.com>
2021-01-21 16:05:24 +0900

    feat:予定日が近い・遠い順で並び替えできる機能を追加

--- app/controllers/datespots_controller.rb
+++ app/controllers/datespots_controller.rb
@@ -38,7 +38,7 @@ class DatespotsController < ApplicationController
         @datespots = @search.result.tagged_with("#{params[:tag_name]}").with_attached_images.preload(:user, :taggings).paginate(page: params[:page], per_page: 9)
       end
     else
-      params[:q] = { sorts: 'updated_at desc' }
+      params[:q] = { sorts: 'date asc' }
       @search = Datespot.where.not(user_id: current_user.id).ransack(params[:q])
       @datespots = @search.result.with_attached_images.preload(:user, :taggings, :comments).paginate(page: params[:page], per_page: 9)
       if params[:tag_name]
