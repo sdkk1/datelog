26db45d9e4e545918bcfadd88eb34b452b28db58
Keita Nishimura <fast.break.baske03@gmail.com>
2021-01-22 18:27:41 +0900

    perf:通知一覧ページの情報を減らし、ユーザーアイコンが表示されるように変更

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -2,7 +2,7 @@ class NotificationsController < ApplicationController
   before_action :logged_in_user
 
   def index
-    @notifications = current_user.notifications.preload(datespot: { images_attachments: :blob }).paginate(page: params[:page], per_page: 5).sort_desc
+    @notifications = current_user.notifications.preload(datespot: { images_attachments: :blob }).paginate(page: params[:page], per_page: 10).sort_desc
     current_user.update_attribute(:notification, false)
   end
 
