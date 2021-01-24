aae00628dd2b6a8a0ee4a43e974192ff8bf39bde
Keita Nishimura <fast.break.baske03@gmail.com>
2021-01-24 14:50:47 +0900

    feat:通知一覧ページのページネーションをkaminariの仕様に変更

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -2,7 +2,8 @@ class NotificationsController < ApplicationController
   before_action :logged_in_user
 
   def index
-    @notifications = current_user.notifications.preload(datespot: { images_attachments: :blob }).paginate(page: params[:page], per_page: 10).sort_desc
+    @notifications_all = current_user.notifications.preload(datespot: { images_attachments: :blob }).sort_desc
+    @notifications = Kaminari.paginate_array(@notifications_all).page(params[:page]).per(10)
     current_user.update_attribute(:notification, false)
   end
 
